30c4c6f8164bd8ae0c50cd82c38a100293e37fe5
ohsarang <delay49@naver.com>
2023-04-24 23:06:36 +0900

    Week_2

--- src/main/java/MethodsExample.java
+++ src/main/java/MethodsExample.java
@@ -1,13 +1,17 @@
 public class MethodsExample {
 
-    public static int main (String[] args){
+    public static void main(String[] args) {
 
+        int mod_result = mod(3, 2);
+        System.out.println(mod_result);
+
+    }
         //함수(메서드)
         // 어떠한 값이 넘겨지거나 아무 값도 넘겨지지 않았을 때
         // 작업을 수행한 후 반환하거나
         // 혹은 반환하지 않고 종료
 
-        int mod_result = mod(a:3, b:2);
+//        int mod_result = mod(a:3, b:2);
 
         // 1. 입력 o , 출력(데이터반환) o
         static int mod(int a, int b) {
@@ -31,6 +35,6 @@ public class MethodsExample {
         }
 
 
-    }
+
 
 }
